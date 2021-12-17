0558687418bd1b1bcebfe5ae88557b242ef138a0
kumshubh99 <93115869+kumshubh99@users.noreply.github.com>
2021-12-17 18:08:40 +0530

    cc
    ccc

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import AddDealer from "./components/controller/DealerController/AddDealer";
+const App = () => {
+  return (
+    <div className="bg-light">
+      {/* <Routes /> */}
+      {/* <farm /> */}
+      <AddDealer/>
+
+    </ div>
+  );
+}
+
+export default App;
+
